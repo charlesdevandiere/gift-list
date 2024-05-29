794ad87b9b18f8ea01a9848ca6adaea14fb967df
Charles de Vandière <charlesdevandiere@users.noreply.github.com>
2024-05-29 22:07:30 +0200

    helmet

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,38 +1,28 @@
-import express, { Request, Response, json, urlencoded } from 'express'
-import passport from 'passport'
-import { BasicStrategy, BasicVerifyFunction } from 'passport-http'
-import { findByToken } from './auth/db'
-import morgan from 'morgan'
 import dotenv from 'dotenv'
-import { authenticate } from './auth'
+import express, { json, urlencoded } from 'express'
 import helmet from 'helmet'
+import morgan from 'morgan'
+import passport from 'passport'
+import { BasicStrategy } from 'passport-http'
+import { authenticate } from './auth'
 
 dotenv.config()
-
-const verify: BasicVerifyFunction = (username: string, password: string, done: (error: any, user?: any) => void) => {
-  authenticate(username, password, (err, user) => {
-    if (err) { return done(err) }
-    if (!user) { return done(null, false) }
-    return done(null, user)
-  }).then()
-}
 
 passport.use(new BasicStrategy(authenticate))
 
 const app = express()
-const port = process.env.PORT
-
 app.use(helmet())
 app.use(morgan('combined'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
 
 app.get('/',
-  passport.authenticate('basic', { session: false }),
-  (req, res) => {
-    res.json(req.user)
-  })
+passport.authenticate('basic', { session: false }),
+(req, res) => {
+  res.json(req.user)
+})
 
+const port = process.env.PORT
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
 })
