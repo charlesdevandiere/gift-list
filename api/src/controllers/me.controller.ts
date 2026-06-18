b2ad54cd8f158a27ee0d3798d121a2ba91d88216
Charles de Vandière <charlesdevandiere@users.noreply.github.com>
2026-06-18 21:49:22 +0200

    feat(api): join group

--- api/src/controllers/me.controller.ts
+++ api/src/controllers/me.controller.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { authenticate, AuthenticatedUsed } from '../auth'
 import { db } from '../db'
 import { User } from '../generated/client'
+import { logger } from '../logger'
 
 export const meController = Router()
 
@@ -64,4 +65,55 @@ meController.get(
     })
 
     res.send(cart.map(item => ({ ...item.user, order: item.order, gifts: item.user.gifts })).filter(item => item.gifts.length))
+  })
+
+// join group
+meController.post(
+  '/me/join',
+  authenticate('user'),
+  async (req, res) => {
+    const group: string = (req.user as AuthenticatedUsed).group
+    const userId: string | undefined = (req.user as AuthenticatedUsed).id
+
+    if (!userId) {
+      res.status(403).send()
+      return
+    }
+
+    if ((await db.user.count({ where: { id: userId } })) == 0) {
+      res.status(404).send()
+      return
+    }
+
+    if (await db.usersOnGroups.count({ where: { userId: userId, groupName: group } })) {
+      // user already in group
+      res.status(204).send()
+      return
+    }
+
+    if (process.env.MAX_NUMBER_OF_USERS_PER_GROUP) {
+      const count: number = await db.user.count({ where: { groups: { some: { groupName: group } } } })
+      if (count >= +process.env.MAX_NUMBER_OF_USERS_PER_GROUP) {
+        res.status(400).send({ error: 'the maximum number of users for this group has already been reached' })
+        return
+      }
+    }
+
+    const order: number = (await db.usersOnGroups.findFirst({
+      where: { groupName: group },
+      orderBy: { order: 'desc' },
+      select: { order: true }
+    }))?.order ?? 0
+
+    await db.usersOnGroups.create({
+      data: {
+        order: order,
+        groupName: group,
+        userId: userId
+      }
+    })
+
+    logger.info(`User '${userId}' added to group ${group}`)
+
+    res.status(204).send()
   })
